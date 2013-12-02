c20e8e608550cc462e7b882e5a37a1357bc1bbdf
bp7860 <bp7860@iozak.com>
2013-12-02 13:05:34 +0100

    Test 50

--- scripts/main.js
+++ scripts/main.js
@@ -1,30 +1,87 @@
-//NOTE: Cordova File api has some issues with file reading in iOS 6
+// Wait for Cordova to load
+//
 document.addEventListener("deviceready", onDeviceReady, false);
-//Activate :active state
-document.addEventListener("touchstart", function() {}, false);
 
-var fileName = 'camp.data';
-var Items = null;
-
+// Cordova is ready
+//
 function onDeviceReady() {
-	
-	showList();
-
-
-
-	function showList() {
-	//var json = jQuery.parseJSON(localStorage.getItem("campingplaete"));
-	//window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFSR, fail);
-	//console.log( localStorage.getItem("campingplaete") );
-	//console.log( jQuery.parseJSON(localStorage.getItem("campingplaete")) );
-
-
-	$.each(Items, function (i, item) {
-		//console.log(i + " - " + item.name);
-		$('#campingplatzelist').append('<li><a href="#campingplaetzedetails-page?id=' + i + '"><img src="data:image/jpg;base64,' + item.image + '" /><h1>' + item.name + '</h1><p>' + item.address + '</p></a></li>');
-	});
-	$("#campingplatzelist").listview("refresh");
-
+	window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
 }
 
-onDeviceReady();
+function gotFS(fileSystem) {
+	fileSystem.root.getFile("readme.txt", {create: true, exclusive: false}, gotFileEntry, fail);
+}
+
+function gotFileEntry(fileEntry) {
+	fileEntry.createWriter(gotFileWriter, fail);
+}
+
+function gotFileWriter(writer) {
+	writer.onwriteend = function(evt) {
+		console.log("contents of file now 'some sample text'");
+		writer.truncate(11);  
+		writer.onwriteend = function(evt) {
+			console.log("contents of file now 'some sample'");
+			writer.seek(4);
+			writer.write(" different text");
+			writer.onwriteend = function(evt){
+				console.log("contents of file now 'some different text'");
+			}
+		};
+	};
+	writer.write("some sample text");
+}
+
+function fail(error) {
+	console.log(error.code);
+}
+
+
+
+
+// Wait for Cordova to load
+//
+function onLoad() {
+	document.addEventListener("deviceready", onDeviceReady, false);
+}
+
+// Cordova is ready
+//
+function onDeviceReady() {
+	window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
+}
+
+function gotFS(fileSystem) {
+	fileSystem.root.getFile("readme.txt", null, gotFileEntry, fail);
+}
+
+function gotFileEntry(fileEntry) {
+	fileEntry.file(gotFile, fail);
+}
+
+function gotFile(file){
+	readDataUrl(file);
+	readAsText(file);
+}
+
+function readDataUrl(file) {
+	var reader = new FileReader();
+	reader.onloadend = function(evt) {
+		console.log("Read as data URL");
+		console.log(evt.target.result);
+	};
+	reader.readAsDataURL(file);
+}
+
+function readAsText(file) {
+	var reader = new FileReader();
+	reader.onloadend = function(evt) {
+		console.log("Read as text");
+		console.log(evt.target.result);
+	};
+	reader.readAsText(file);
+}
+
+function fail(evt) {
+	console.log(evt.target.error.code);
+}
