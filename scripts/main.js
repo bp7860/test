158b3a4a245ee16575c07429ac96d02d76572d37
bp7860 <bp7860@iozak.com>
2013-12-02 14:44:00 +0100

    Test 56

--- scripts/main.js
+++ scripts/main.js
@@ -1,27 +1,28 @@
+$('#status2').html('Ready1');
 //NOTE: Cordova File api has some issues with file reading in iOS 6
 document.addEventListener("deviceready", onDeviceReady, false);
 //Activate :active state
 document.addEventListener("touchstart", function() {}, false);
 
 function onDeviceReady() {
-	$('status2').html('Ready');
+	$('#status2').html('Ready2');
    fileSystemHelper.writeLine('readme.txt', 'text1', 
-   	function() {$('status').html('Geschrieben');}, function() {});
+   	function() {$('#status').html('Geschrieben');}, function() {});
 
    navigator.notification.alert(
         'test',  // message
-        alertDismissed,         // callback
+        alertDismissed         // callback
     );
 
    window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fs) {
-   			$('status').html($('status').html()+fs.root.fullPath);
+   			$('#status').html($('#status').html()+fs.root.fullPath);
             console.log("Root = " + fs.root.fullPath);
             var directoryReader = fs.root.createReader();
             directoryReader.readEntries(function(entries) {
                 var i;
                 for (i=0; i<entries.length; i++) {
                     console.log(entries[i].name);
-                	$('status').html($('status').html()+entries[i].name);
+                	$('#status').html($('#status').html()+entries[i].name);
 
                 }
             }, function (error) {
