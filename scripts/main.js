73056e0f4e77759e88ac25e57f5e4210ddb68d01
bp7860 <bp7860@iozak.com>
2013-12-02 14:33:03 +0100

    Test 54

--- scripts/main.js
+++ scripts/main.js
@@ -5,12 +5,30 @@ document.addEventListener("touchstart", function() {}, false);
 
 function onDeviceReady() {
    fileSystemHelper.writeLine('readme.txt', 'text1', 
-   	function() {$('status').html('Geschrieben')}, function() {});
+   	function() {$('status').html('Geschrieben');}, function() {});
 
    navigator.notification.alert(
         'test',  // message
         alertDismissed,         // callback
     );
+
+   window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fs) {
+   			$('status').html($('status').html()+fs.root.fullPath);
+            console.log("Root = " + fs.root.fullPath);
+            var directoryReader = fs.root.createReader();
+            directoryReader.readEntries(function(entries) {
+                var i;
+                for (i=0; i<entries.length; i++) {
+                    console.log(entries[i].name);
+                	$('status').html($('status').html()+entries[i].name);
+
+                }
+            }, function (error) {
+                alert(error.code);
+            })
+       }, function (error) {
+               alert(error.code);
+       });
 }
 
 function FileApp() {
