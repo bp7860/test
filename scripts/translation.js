6a9e930d7f1609b9d73a93905312b396231416bd
unknown <p.malleier@WS20.web-crossing.local>
2014-02-20 17:06:27 +0100

    Test 462

---
+++ scripts/translation.js
@@ -0,0 +1,49 @@
+var _l = new Array(
+		new Array( "Information", "Kontakt", "Ausstattung", "Info", "Preise", "Impressionen", "Erwachsene", "Kinder", "Auto", "Wohnanhänger", "Wohnmobil", "großes Zelt", "Zelt",
+					"Motorrad", "Hunde", "Strom", "Home", "Service", "Details", "Impressum", "Partner", "Weitere Services", "Update", "Anfahrt", "Region", "Verkehr", "Camping Südtirol", "Campingplatz", "Wetter", "Film" ),
+
+		new Array( "Information", "Contact", "Equipment", "Description", "Prices", "Impressions", "Adults", "Kids", "Car", "Caravan", "Camper", "Marquee", "Tent",
+					"Motorbike", "Dog", "Electricity", "Home", "Service", "Details", "Imprint", "Partners", "More Services", "Update", "Map", "Region", "Traffic", "Camping South Tyrol", "campsite", "weather", "Movie" ),
+		new Array(),
+		new Array( "Informazioni", "Contatto", "Attrezzatura", "Annotazione", "Prezzi", "Impressioni", "Adulti", "Bambini", "Macchina", "Caravan", "Camper", "Grande tenda", "Tenda",
+					"Moto", "Cani", "Corrente", "Home", "Servizio", "Dettagli", "Informazioni legali", "Partner", "altri servizi", "Aggiornamento", "Arrivo", "Regione", "traffico", "Camping Alto Adige", "campeggio", "meteo", "film" ),
+		new Array(),
+		new Array(),
+		new Array( "Informatie", "Contact", "Uitrusting", "Opmerking", "Prijzen", "Impressies", "Volwassenen", "Kinderen", "Auto", "Caravan", "Camper", "Grote tent", "Tent",
+					"Motor", "Honden", "Stroom", "Home", "Service", "Details", "Impressum", "Partner", "Andere services", "Update", "Route", "Regio", "Verkeer", "Camping Südtirol", "Campingplaatsen", "Weer", "film" )
+	);
+
+	if( localStorage.getItem("lang") === null ) {
+		language = navigator.language.split("-"); 
+	    language_root = (language[0]);
+
+	    if( language_root == "en" )
+	    	Lang = 1;
+	    else if( language_root == "it" )
+	    	Lang = 3;
+	    else if( language_root == "nl" )
+	    	Lang = 6;
+
+	    localStorage.setItem( "lang", Lang );
+	}
+	else {
+		Lang = localStorage.getItem("lang");
+	}
+
+	Lang = 6;
+
+	/* Translate */
+	//if( Lang != "de" ) {
+		$('#lng_camping').html(_l[Lang][27]);
+		$('#lng_campingsuedtirol').html(_l[Lang][26]);
+		$('#lng_home').html(_l[Lang][16]);
+		$('#lng_service').html(_l[Lang][17]);
+		$('#lng_details').html(_l[Lang][18]);
+		$('#lng_back').html(_l[Lang][18]);
+		$('#lng_impressum').html(_l[Lang][5]);
+		$('#lng_partner').html(_l[Lang][20]);
+		$('#lng_moreservices').html(_l[Lang][21]);
+		$('#lng_wetter').html(_l[Lang][28]);
+		$('#lng_verkehr').html(_l[Lang][25]);
+		$('#lng_film').html(_l[Lang][29]);
+	//}
